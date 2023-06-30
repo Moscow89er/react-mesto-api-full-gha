f8a9e698c8ff51e337a487e876a369cbb6934221
Nikita Khubaev <khubaev.n@yandex.ru>
2023-06-30 12:56:55 +0400

    добавил значение по умолчанию для JWT

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV = 'development', JWT_SECRET = 'my-secret-key' } = process.env;
 
 module.exports = {
   NODE_ENV,
